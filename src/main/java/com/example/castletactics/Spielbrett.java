58d15b357199f0dbc55d63bc39249029d7c1a9d8
Timo Buschmann <timo.buschmann@peergroup.com>
2023-06-12 11:56:22 +0200

    Spielbrett grafisch eingebunden (nicht optimal, aber geht)

--- src/main/java/com/example/castletactics/Spielbrett.java
+++ src/main/java/com/example/castletactics/Spielbrett.java
@@ -1,6 +1,7 @@
 package com.example.castletactics;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -10,26 +11,31 @@ public class Spielbrett extends Tactics {
     public void start(Stage primaryStage) {
         // Create a GridPane
         GridPane pane = new GridPane();
+        Button schließenBTN = new Button("Schließen");
+        schließenBTN.setOnAction(l-> new Tactics().start(primaryStage));
 
         // Create 64 rectangles and add to pane
         int count = 0;
-        double s = 100; // side of rectangle
+        double s = 50; // side of rectangle
         for (int i = 0; i < 8; i++) {
             count++;
             for (int j = 0; j < 8; j++) {
                 Rectangle r = new Rectangle(s, s, s, s);
-                if (count % 2 == 0)
+                if (count % 2 == 1)
                     r.setFill(Color.WHITE);
                 pane.add(r, j, i);
                 count++;
             }
         }
 
+        pane.add(schließenBTN, 0, 8, 2, 1);
+
         // Create a scene and place it in the stage
         Scene scene = new Scene(pane);
         primaryStage.setTitle("Castle Tactics"); // Name des pane gesetzt
         primaryStage.setScene(scene); //  scene in  primaryStage gesetzt
         primaryStage.show();
+
     }
 
 }
--- src/main/java/com/example/castletactics/Tactics.java
+++ src/main/java/com/example/castletactics/Tactics.java
@@ -1,7 +1,10 @@
 package com.example.castletactics;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -10,10 +13,15 @@ import java.io.IOException;
 
 public class Tactics extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        StackPane root = new StackPane();
+    public void start(Stage stage) {
+        Group root = new Group();
         Scene scene = new Scene(root,500,500);
         //css
+
+        Button spielbrettBTN = new Button("Spielbrett öffnen");
+        spielbrettBTN.setOnAction(l-> new Spielbrett().start(stage));
+        root.getChildren().add(spielbrettBTN);
+
         scene.getStylesheets().add("/style.css");
         //Scene wird erstellt
         stage.setScene(scene);
@@ -21,6 +29,7 @@ public class Tactics extends Application {
         stage.setResizable(false);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
