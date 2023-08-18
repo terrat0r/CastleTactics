3d2a77e1cebea8173d84ecefe656df44cb5be56c
Lucas <lucas.schmidt1001@gmail.com>
2023-08-18 10:57:51 +0200

    änderungen an zugmechanik

--- src/main/java/com/example/castletactics/Läufer.java
+++ src/main/java/com/example/castletactics/Läufer.java
@@ -2,9 +2,17 @@ package com.example.castletactics;
 
 import javafx.scene.layout.GridPane;
 
+import java.util.List;
+
 public class Läufer extends Figur {
 	Läufer(GridPane pane, boolean isWhite, int side, int col, int row, Spielverwaltung spv){
 		super(pane, isWhite, isWhite ? "file:./src/main/resources/img/chess-bishop-white.png" : "file:./src/main/resources/img/chess-bishop-black.png", side, col, row, spv);
+	}
+
+
+	@Override
+	public List<predict> getPossibleMoves() {
+		return null;
 	}
 
 	@Override
--- src/main/java/com/example/castletactics/Springer.java
+++ src/main/java/com/example/castletactics/Springer.java
@@ -2,9 +2,17 @@ package com.example.castletactics;
 
 import javafx.scene.layout.GridPane;
 
+import java.util.List;
+
 public class Springer extends Figur {
 	Springer(GridPane pane, boolean isWhite, int side, int col, int row, Spielverwaltung spv){
 		super(pane, isWhite, isWhite ? "file:./src/main/resources/img/chess-knight-white.png" : "file:./src/main/resources/img/chess-knight-black.png", side, col, row, spv);
+	}
+
+
+	@Override
+	public List<predict> getPossibleMoves() {
+		return null;
 	}
 
 	@Override
