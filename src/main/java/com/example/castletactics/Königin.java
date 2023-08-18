c8c6d0df6f06cb9364abd63f95bc6115d9dcd464
Timo Buschmann <timo.buschmann@peergroup.com>
2023-08-18 14:04:00 +0200

    Läufer- und Königinlogik gefixt (hoffentlich)

--- src/main/java/com/example/castletactics/Königin.java
+++ src/main/java/com/example/castletactics/Königin.java
@@ -14,61 +14,51 @@ public class Königin extends Figur {
 		return null;
 	}
 
+
 	@Override
 	public boolean zugErlaubt(int row, int col, int rowDest, int colDest) {
-		// Check if the source and destination positions are the same
-		if (row == rowDest && col == colDest) {
-			return false;
-		}
-
-		// Check horizontal movement
-		if (row == rowDest) {
-			int startCol = Math.min(col, colDest);
-			int endCol = Math.max(col, colDest);
-			for (int c = startCol + 1; c < endCol; c++) {
-				// Check for obstructions
-				if (spv.figuren[row][c] != null) {
-					return false;
-				}
+		if (Math.abs(row - rowDest) == Math.abs(col - colDest)  || row == rowDest || col == colDest) {
+			int rowFactor;
+			if(row < rowDest) {
+				rowFactor = 1;
 			}
-			return true;
-		}
-
-		// Check vertical movement
-		if (col == colDest) {
-			int startRow = Math.min(row, rowDest);
-			int endRow = Math.max(row, rowDest);
-			for (int r = startRow + 1; r < endRow; r++) {
-				// Check for obstructions
-				if (spv.figuren[r][col] != null) {
-					return false;
-				}
+			else if (row == rowDest)
+			{
+				rowFactor = 0;
 			}
-			return true;
-		}
+			else {
+				rowFactor = -1;
+			}
+			int colFactor;
 
-		// Check diagonal movement
-		if (Math.abs(row - rowDest) == Math.abs(col - colDest)) {
-			int startRow = Math.min(row, rowDest);
-			int endRow = Math.max(row, rowDest);
-			int startCol = Math.min(col, colDest);
-			int endCol = Math.max(col, colDest);
-			int r = startRow + 1;
-			int c = startCol + 1;
-			while (r < endRow && c < endCol) {
-				// Check for obstructions on the board
+			if(col < colDest) {
+				colFactor = 1;
+			}
+			else if (col == colDest)
+			{
+				colFactor = 0;
+			}
+			else {
+				colFactor = -1;
+			}
+			// Sich selbst muss man nicht prüfen
+			int r = row + rowFactor;
+			int c = col + colFactor;
+			while (r != rowDest && c != colDest) {
+				// Check for obstructions on the way
 				if (spv.figuren[r][c] != null) {
 					return false;
 				}
-				r++;
-				c++;
+				r += rowFactor;
+				c += colFactor;
 			}
-			return true;
+			// Check for destination
+			if (spv.figuren[r][c] == null || spv.figuren[r][c] != null && spv.figuren[r][c].isWhite != this.isWhite) {
+				return true;
+			}
 		}
-
 		// The move is not allowed
 		return false;
 	}
-
 
 }
--- src/main/java/com/example/castletactics/Läufer.java
+++ src/main/java/com/example/castletactics/Läufer.java
@@ -18,21 +18,36 @@ public class Läufer extends Figur {
 	@Override
 	public boolean zugErlaubt(int row, int col, int rowDest, int colDest) {
 		if (Math.abs(row - rowDest) == Math.abs(col - colDest)) {
-			int startRow = Math.min(row, rowDest);
-			int endRow = Math.max(row, rowDest);
-			int startCol = Math.min(col, colDest);
-			int endCol = Math.max(col, colDest);
-			int r = startRow + 1;
-			int c = startCol + 1;
-			while (r < endRow && c < endCol) {
-				// Check for obstructions on the board
+			int rowFactor;
+			if(row < rowDest) {
+				rowFactor = 1;
+			}
+			else {
+				rowFactor = -1;
+			}
+			int colFactor;
+			if(col < colDest) {
+				colFactor = 1;
+			}
+			else {
+				colFactor = -1;
+			}
+			int r = row + rowFactor;
+			int c = col + colFactor;
+
+			while (r != rowDest && c != colDest) {
+				// Check for obstructions on the way
 				if (spv.figuren[r][c] != null) {
 					return false;
 				}
-				r++;
-				c++;
+				r += rowFactor;
+				c += colFactor;
 			}
-			return true;
+
+			// Check for destination
+			if (spv.figuren[r][c] == null || spv.figuren[r][c] != null && spv.figuren[r][c].isWhite != this.isWhite) {
+				return true;
+			}
 		}
 
 		// The move is not allowed
