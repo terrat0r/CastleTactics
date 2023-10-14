e9bc432797cb146e37a7ecdd5856c4cb02f83adc
elblorenz32 <timo-buschmann.tb@gmx.de>
2023-10-14 15:23:46 +0200

    Empfängermethode eingebaut

--- src/main/java/com/example/castletactics/Spielverwaltung.java
+++ src/main/java/com/example/castletactics/Spielverwaltung.java
@@ -3,6 +3,9 @@ package com.example.castletactics;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class Spielverwaltung extends Application{
 	public final Spielbrett brett;
@@ -39,6 +42,24 @@ public class Spielverwaltung extends Application{
 	public static void main(String[] args) {
 		launch();
 		//new Spielverwaltung();
+	}
+
+
+	//TODO: send Move
+
+	public boolean receiveMove(String move) {
+		Pattern pattern = Pattern.compile("^(\\w*).*?\\[(\\d)[,;|] ?(\\d)] (?:nach|to) \\[(\\d)[,;|] ?(\\d)]"); //z.B. Bauer von [1,2] nach [2, 3]
+		Matcher matcher = pattern.matcher(move);
+		if (matcher.find()) { // Match gefunden
+			String typ = matcher.group(1);
+			int row = Integer.parseInt(matcher.group(2));
+			int col = Integer.parseInt(matcher.group(3));
+			int rowDest = Integer.parseInt(matcher.group(4));
+			int colDest = Integer.parseInt(matcher.group(5));
+			zugverwaltung.figuren[row][col].move(rowDest, colDest);
+			return true;
+		}
+		return false;
 	}
 
 }
